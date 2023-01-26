06cc98dd63f0962957389f2d663ced8ce45d4d6d
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-01-26 09:35:42 -0500

    Initial Commit

---
+++ TerminologyRecap/TerminologyRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminologyRecapApp.swift
+//  TerminologyRecap
+//
+//  Created by Oliver Finlayson on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct TerminologyRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
